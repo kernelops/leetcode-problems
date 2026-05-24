55f20b75e9a006201cb466c4bb7c2b30fc7ae5cd
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-05-25 00:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (47.15%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string customSortString(string order, string s) 
+    {
+        map<char, int> mpp; 
+        for (char c: s) mpp[c]++; 
+
+        string ans=""; 
+        for (char c: order){
+            int count = mpp[c]; 
+            for (int i=0; i<count; i++){
+                ans += c; 
+                mpp[c]--; 
+            }
+            mpp.erase(c); 
+        }    
+
+        if (!mpp.empty()){
+            for (auto i: mpp){ 
+                for (int j=0; j<i.second; j++){
+                    ans += i.first; 
+                    mpp[i.second]--; 
+                }
+                mpp.erase(i.first); 
+            }
+        }
+
+        return ans; 
+    }
+};
