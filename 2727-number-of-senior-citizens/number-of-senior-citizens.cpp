9cd924efab7c2f2d93d5291ae7853244908d1784
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-05-24 15:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (11.02%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) 
     {
-        vector<int> age;  
-        for (string s: details){
-            string a="";
-            a += s[11]; 
-            a += s[12];  
-            age.push_back(stoi(a)); 
-        }    
+        int count = 0;
 
-        int count=0; 
-        for (int a: age)
-            if (a>60) count++; 
-        
-        return count; 
+        for (string s : details) {
+            int age = stoi(s.substr(11, 2));
+
+            if (age > 60)
+                count++;
+        }
+
+        return count;
     }
 };
