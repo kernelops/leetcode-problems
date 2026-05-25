722aaa626c7695ec0906fe21751286f9c538937e
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-05-26 00:22:16 +0530

    Time: 3 ms (13.37%) | Memory: 21.3 MB (6.03%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) 
+    {
+        if (nums.size() == 1) return nums; 
+
+        vector<int> even; vector<int> odd; 
+        for (int i: nums){
+            if (i%2 == 0) even.push_back(i);
+            else odd.push_back(i); 
+        }    
+
+        vector<int> ans; 
+        for (int i: even) ans.push_back(i); 
+        for (int i: odd) ans.push_back(i); 
+
+        return ans; 
+    }
+};
