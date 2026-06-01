e5d42e5a54478213e8ea294c66d6ba6be3ba24b7
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-01 23:57:37 +0530

    Time: 33 ms (83.97%) | Memory: 88.8 MB (12.06%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) 
+    {
+        int left=0, right=0, count=0;
+        map<int, int> mpp;  
+        while (right < fruits.size()){
+            mpp[fruits[right]]++; 
+            while (mpp.size() > 2){
+                mpp[fruits[left]]--;  
+                if (mpp[fruits[left]] == 0) mpp.erase(fruits[left]);
+                left++; 
+            }
+
+            count = max(count, right-left+1); 
+            right++;    
+        }
+
+        return count; 
+    }
+};
