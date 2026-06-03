25cdb8ef50587adfaa5c2fc3f2288923dade324b
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:33:54 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/3/2026, 11:33:54 PM
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
