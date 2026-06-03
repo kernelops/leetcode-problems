df0a1f41dd5e77d77fbc9fcf5af7d043a2e46a0c
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:33:49 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.cpp
@@ -0,0 +1,47 @@
+// Last updated: 6/3/2026, 11:33:48 PM
+class Solution {
+public:
+    string simplifyPath(string path) 
+    {
+        string token=""; 
+        stack<string> st; 
+
+        for (char c: path){
+            if (c == '/'){
+                if (!token.empty()){
+                    if (token == "."){}
+                    else if (token == ".."){
+                        if (!st.empty()) st.pop(); 
+                    }
+                    else st.push(token); 
+
+                    token.clear(); 
+                }
+            }
+            else token += c; 
+        }  
+
+        if (!token.empty()){
+            if (token == "."){}
+            else if (token == ".."){
+                if (!st.empty()) st.pop(); 
+            }
+            else st.push(token);            
+        }  
+
+        if (st.empty()) return "/";
+        
+        vector<string> dir; 
+        while (!st.empty()){
+            dir.push_back(st.top()); 
+            st.pop(); 
+        }
+        reverse(dir.begin(), dir.end()); 
+
+        string ans=""; 
+        for (auto item: dir)
+            ans += "/" + item; 
+
+        return ans; 
+    }
+};
