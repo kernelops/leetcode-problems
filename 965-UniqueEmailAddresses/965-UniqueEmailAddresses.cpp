ebc0e3396b35a03379b15250cdc5932229963212
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:30:13 +0530

    Create 965-UniqueEmailAddresses

---
+++ 965-UniqueEmailAddresses/965-UniqueEmailAddresses.cpp
@@ -0,0 +1,26 @@
+// Last updated: 6/3/2026, 11:30:13 PM
+class Solution {
+public: 
+    int numUniqueEmails(vector<string>& emails) 
+    {
+        map<string, int> mpp;
+        for (string s: emails){
+            string temp=""; bool at = false, plus = false; 
+            for (char c: s){
+                if (at == false && c == '.') continue;
+                else if (at == true && c == '.') temp += c; 
+                else if (c == '+'){
+                    plus = true; 
+                    continue;   
+                }
+                else if (at == false && plus == false && isalnum(c)) temp += c; 
+                else if (plus == false && isalnum(c)) temp += c; 
+                else if (at == true) temp+=c;  
+                else if (c == '@') {temp += c; at = true; }
+            }
+            cout << temp; 
+            mpp[temp]++; 
+        }    
+        return mpp.size(); 
+    }
+};
