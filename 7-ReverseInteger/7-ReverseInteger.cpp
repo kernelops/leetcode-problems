58f602fdca75459f2a07cf7846900939af19e979
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:35:01 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,39 @@
+// Last updated: 6/3/2026, 11:35:00 PM
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        long long sum=0;
+        if (x>=0)
+        {
+            string s = to_string(x);
+            int n = s.size()-1;
+            while (x>0)
+            {
+                int d = x%10;
+                sum = sum + pow(10, n) * d;
+                n--;
+                x = x/10;
+            }
+        }
+        else
+        {
+            long long temp = x;
+            temp = abs(temp);
+            string s = to_string(temp);
+            int n = s.size()-1;
+            while (temp>0)
+            {
+                int d = temp%10;
+                sum = sum + pow(10, n) * d;
+                n--;
+                temp = temp/10;
+            }
+            sum = sum*-1;           
+        }
+        if (sum > INT_MAX || sum < INT_MIN)
+            return 0;
+
+        return sum;    
+    }
+};
