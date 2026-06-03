38b2e5a87a7545ec657fe3f51f86629a6ad1a8f1
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:29:03 +0530

    Create 3447-ClearDigits

---
+++ 3447-ClearDigits/3447-ClearDigits.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/3/2026, 11:29:03 PM
+class Solution {
+public:
+    string clearDigits(string s) 
+    {
+        stack<char> st; 
+        for (char c: s){
+            if (isdigit(c)){
+                if (!st.empty()) st.pop(); 
+            }
+            else st.push(c); 
+        }     
+
+        string ans=""; 
+        while (!st.empty()){
+            ans += st.top(); 
+            st.pop(); 
+        }
+        reverse(ans.begin(), ans.end()); 
+
+        return ans; 
+    }
+};
