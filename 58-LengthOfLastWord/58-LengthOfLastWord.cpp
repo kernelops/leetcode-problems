243c8d2016c4ad175bbdccedc77037b6df181f92
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:33:59 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/3/2026, 11:33:59 PM
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        vector<string> words; 
+        int ptr=0; 
+        while (ptr < s.size()){ 
+            string word = ""; 
+            if (s[ptr] != ' '){
+                while (ptr < s.size() && s[ptr] != ' '){
+                    word += s[ptr]; 
+                    ptr++; 
+                }
+                words.push_back(word); 
+            }
+            ptr++; 
+        }
+        return words[words.size()-1].size(); 
+    }
+};
