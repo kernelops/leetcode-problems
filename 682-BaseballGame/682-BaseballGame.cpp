78c8a5715ef1bc6f61ac50cbebfd5bcff8001911
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:30:45 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.cpp
@@ -0,0 +1,29 @@
+// Last updated: 6/3/2026, 11:30:44 PM
+class Solution {
+public:
+    int calPoints(vector<string>& operations) 
+    {
+        stack<int> st; 
+        for (string s: operations){
+            if (s == "C"){
+                if (!st.empty()) st.pop(); 
+            }
+            else if (s == "D") st.push(st.top()*2); 
+            else if (s == "+"){
+                int num1 = st.top(); st.pop(); 
+                int num2 = st.top(); st.pop(); 
+                st.push(num2); 
+                st.push(num1); 
+                st.push(num1+num2); 
+            }
+            else st.push(stoi(s)); 
+        }    
+
+        int sum=0;
+        while (!st.empty()){
+            sum += st.top(); st.pop(); 
+        }
+
+        return sum; 
+    }
+};
