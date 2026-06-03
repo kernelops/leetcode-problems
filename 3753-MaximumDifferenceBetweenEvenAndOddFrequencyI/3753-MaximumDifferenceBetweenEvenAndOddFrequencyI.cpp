f52256eb6e8c272890c742a261cacf5484920a59
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:29:05 +0530

    Create 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI

---
+++ 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI/3753-MaximumDifferenceBetweenEvenAndOddFrequencyI.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/3/2026, 11:29:05 PM
+class Solution {
+public:
+    int maxDifference(string s) 
+    {
+        map<char, int> mpp; 
+        for (char c: s) mpp[c]++; 
+
+        vector<int> freq;
+        for (auto i: mpp)    
+            freq.push_back(i.second); 
+        
+        int maxo=INT_MIN, mino=INT_MAX, maxe=INT_MIN, mine=INT_MAX; 
+        for (int i: freq){
+            if (i%2 == 0)
+                mine = min(mine, i);
+            else
+                maxo = max(maxo, i); 
+        }
+
+        return maxo-mine; 
+    }   
+};
