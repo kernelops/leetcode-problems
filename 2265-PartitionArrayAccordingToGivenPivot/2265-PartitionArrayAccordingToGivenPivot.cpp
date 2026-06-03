8751d568e496c1a71fbae30817b904c0c72131e6
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-03 23:29:24 +0530

    Create 2265-PartitionArrayAccordingToGivenPivot

---
+++ 2265-PartitionArrayAccordingToGivenPivot/2265-PartitionArrayAccordingToGivenPivot.cpp
@@ -0,0 +1,25 @@
+// Last updated: 6/3/2026, 11:29:23 PM
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) 
+    {
+        int count_pivot=0; 
+        for (int i: nums) if (i==pivot) count_pivot++; 
+        
+        vector<int> ans; 
+        for (int i: nums){
+            if (i<pivot)
+                ans.push_back(i); 
+        }    
+
+        for (int i=0; i<count_pivot; i++) ans.push_back(pivot); 
+
+        for (int i: nums){
+            if (i>pivot)
+                ans.push_back(i); 
+        } 
+
+        return ans; 
+
+    }
+};
