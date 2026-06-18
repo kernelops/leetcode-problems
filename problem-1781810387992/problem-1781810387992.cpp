0b34eaa75cb8cd607acdfa1266a3c8ac4f016989
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-19 00:49:48 +0530

    Create

---
+++ problem-1781810387992/problem-1781810387992.cpp
@@ -0,0 +1,43 @@
+// Last updated: 6/19/2026, 12:49:47 AM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    long long kthLargestLevelSum(TreeNode* root, int k) 
+15    {
+16        vector<long long> levelSum; 
+17        queue<TreeNode*> q; 
+18        q.push(root); 
+19
+20        while (!q.empty()){
+21            int size = q.size(); long long sum=0; 
+22
+23            for (int i=0; i<size; i++){
+24                TreeNode* node = q.front(); 
+25                q.pop(); 
+26                sum += node->val; 
+27
+28                if (node->left) q.push(node->left); 
+29                if (node->right) q.push(node->right); 
+30            }
+31
+32            levelSum.push_back(sum); 
+33        }    
+34 
+35        if (k > levelSum.size()) return -1; 
+36        //sort(levelSum.begin(), levelSum.end()); // ascending sort
+37        //return levelSum[levelSum.size()-k]; // answer for ascending sort
+38        
+39        sort(levelSum.begin(), levelSum.end(), greater<long long>()); // descending sort
+40        return levelSum[k-1]; // answer for descending sort
+41    }
+42};
