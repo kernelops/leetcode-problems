830a9bc8aeca986bbffe119e93cbbc49d9deeb7f
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-06-26 21:56:37 +0530

    Create 3016-MinimumNumberofPushestoTypeWordII

---
+++ 3016-MinimumNumberofPushestoTypeWordII/version/cpp/3016-MinimumNumberofPushestoTypeWordII_20260626_215635.cpp
@@ -0,0 +1,26 @@
+// Last updated: 6/26/2026, 9:56:35 PM
+1class Solution {
+2public:
+3    int minimumPushes(string word) 
+4    {
+5        unordered_map<char, int> mpp; 
+6        for (char c : word) mpp[c]++; 
+7
+8        if (mpp.size() <= 8) return word.size(); 
+9        
+10        vector<int> count; 
+11        for (auto i: mpp) count.push_back(i.second); 
+12
+13        sort(count.begin(), count.end()); 
+14
+15        int ans=0, val=0, a=0;
+16        for (int i=count.size()-1; i>=0; i--)
+17        {
+18            if (a%8 == 0) val++; 
+19            ans += count[i]*val; 
+20            a++; 
+21        }    
+22
+23        return ans; 
+24    }
+25};
