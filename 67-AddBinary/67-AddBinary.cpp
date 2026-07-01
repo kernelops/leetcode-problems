91f3db093eba32975926ed4306043622eae69010
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-07-01 09:23:35 +0530

    File updated at 7/1/2026, 9:23:34 AM

--- 67-AddBinary/67-AddBinary.cpp
+++ 67-AddBinary/67-AddBinary.cpp
@@ -1,56 +1,25 @@
-// Last updated: 6/3/2026, 11:33:52 PM
-#include <algorithm>
-#include <string>
-using namespace std;
-
-class Solution {
-public:
-    string addBinary(string a, string b) 
-    {
-        int len_a = a.length() - 1;
-        int len_b = b.length() - 1;
-        string sum;
-        int carry = 0;
-        while (len_a >= 0 || len_b >= 0)
-        {
-            int num_a = (len_a >= 0 ? a[len_a] - '0' : 0);
-            int num_b = (len_b >= 0 ? b[len_b] - '0' : 0);
-            if (num_a == 0 && num_b == 0 && carry == 0)
-            {
-                sum.push_back('0');
-                carry = 0;
-            }
-            else if (num_a == 0 && num_b == 0 && carry == 1)
-            {
-                sum.push_back('1');
-                carry = 0;
-            }
-            else if (((num_a == 1 && num_b == 0) || (num_a == 0 && num_b == 1)) && carry == 0)
-            {
-                sum.push_back('1');
-                carry = 0;
-            }
-            else if (((num_a == 1 && num_b == 0) || (num_a == 0 && num_b == 1)) && carry == 1)
-            {
-                sum.push_back('0');
-                carry = 1;
-            }
-            else if (num_a == 1 && num_b == 1 && carry == 0)
-            {
-                sum.push_back('0');
-                carry = 1;
-            }
-            else if (num_a == 1 && num_b == 1 && carry == 1)
-            {
-                sum.push_back('1');
-                carry = 1;
-            }
-            if (len_a >= 0) len_a--;
-            if (len_b >= 0) len_b--;
-        }
-        if (carry == 1)
-            sum.push_back('1');
-        reverse(sum.begin(), sum.end());
-        return sum;
-    }
-};
+// Last updated: 7/1/2026, 9:23:34 AM
+1class Solution {
+2public:
+3    string addBinary(string a, string b) 
+4    {
+5        int pta=a.size()-1, ptb=b.size()-1, sum=0, carry=0;
+6        string ans=""; 
+7
+8        while (pta >= 0 || ptb >= 0 || carry){
+9            int bitA = (pta >= 0) ? a[pta] - '0' : 0; 
+10            int bitB = (ptb >= 0) ? b[ptb] - '0' : 0; 
+11
+12            int sum = bitA + bitB + carry; 
+13
+14            int sumBit = sum % 2; 
+15            carry = sum/2; 
+16
+17            ans.push_back(sumBit ? '1' : '0'); 
+18            pta--; ptb--;  
+19        }  
+20        reverse(ans.begin(), ans.end()); 
+21
+22        return ans;  
+23    }
+24};
