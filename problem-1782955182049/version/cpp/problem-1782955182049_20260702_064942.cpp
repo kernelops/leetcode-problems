c7fcbcd9063dfccc6ffd90dc736986c7adb6c6ae
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-07-02 06:49:42 +0530

    Create

---
+++ problem-1782955182049/version/cpp/problem-1782955182049_20260702_064942.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/2/2026, 6:49:42 AM
+1class Solution {
+2public:
+3    int rangeBitwiseAnd(int left, int right) 
+4    {
+5        int shift = 0; 
+6        while (left != right){
+7            left = left >> 1; 
+8            right = right >> 1; 
+9            shift ++; 
+10        }    
+11
+12        return left << shift; 
+13    }
+14};
