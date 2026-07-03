aeafb6617035d40c4789c31256f8d9293b53088c
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-07-03 18:18:31 +0530

    Create

---
+++ problem-1783082911363/version/cpp/problem-1783082911363_20260703_181831.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/3/2026, 6:18:31 PM
+1class Solution {
+2public:
+3    int findLucky(vector<int>& arr) 
+4    {
+5        unordered_map<int, int> mpp; 
+6        for (int i: arr) mpp[i]++; 
+7
+8        int ans=INT_MIN; 
+9        for (auto i: mpp){
+10            if (i.first == i.second)
+11                ans = max(ans, i.first); 
+12        }    
+13
+14        return (ans == INT_MIN) ? -1 : ans; 
+15    }
+16};
