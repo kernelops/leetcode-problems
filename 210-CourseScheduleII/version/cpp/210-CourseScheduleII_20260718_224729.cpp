d5a55c1ff9a68679b5cb5b8860dbe03daae06b39
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-07-18 22:47:31 +0530

    Create 210-CourseScheduleII

---
+++ 210-CourseScheduleII/version/cpp/210-CourseScheduleII_20260718_224729.cpp
@@ -0,0 +1,38 @@
+// Last updated: 7/18/2026, 10:47:29 PM
+1class Solution {
+2public:
+3    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+4    {
+5        // adjacency list
+6        vector<vector<int>> adj(numCourses); 
+7        for (auto i: prerequisites){
+8            adj[i[1]].push_back(i[0]); 
+9        }    
+10
+11        // compute indegree
+12        vector<int> indegree(numCourses, 0); 
+13        for (auto i: prerequisites)
+14            indegree[i[0]]++;
+15
+16        // push indegree zero to queue
+17        queue<int> q; 
+18        for (int i=0; i<indegree.size(); i++)
+19            if (indegree[i] == 0) q.push(i); 
+20
+21        // topological sort 
+22        vector<int> ans; 
+23        while (!q.empty()){
+24            auto node = q.front(); 
+25            q.pop(); 
+26            ans.push_back(node); 
+27
+28            for (int i: adj[node]){
+29                indegree[i]--; 
+30                if (indegree[i] == 0) q.push(i); 
+31            }
+32        } 
+33
+34        if (ans.size() == numCourses) return ans; 
+35        return {}; 
+36    }
+37};
