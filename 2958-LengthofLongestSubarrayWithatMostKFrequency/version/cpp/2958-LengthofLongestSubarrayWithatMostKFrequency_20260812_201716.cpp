5b5bdeb6274a1734fd41e377e4f5d5f42aea78a7
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-08-12 20:17:17 +0530

    Create 2958-LengthofLongestSubarrayWithatMostKFrequency

---
+++ 2958-LengthofLongestSubarrayWithatMostKFrequency/version/cpp/2958-LengthofLongestSubarrayWithatMostKFrequency_20260812_201716.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/12/2026, 8:17:16 PM
+1class Solution {
+2public:
+3    int maxSubarrayLength(vector<int>& nums, int k) 
+4    {
+5        unordered_map<int, int> mpp; 
+6        for (int i: nums) mpp[i]++; 
+7
+8        unordered_map<int, int> freq; 
+9        int ans=0, l=0; 
+10
+11        for (int r=0; r<nums.size(); r++){
+12            freq[nums[r]]++; 
+13
+14            while (freq[nums[r]] > k){
+15                freq[nums[l]]--; 
+16                if (freq[nums[l]] == 0) freq.erase(nums[l]); 
+17                l++; 
+18            }
+19            ans = max(ans, r-l+1); 
+20        }
+21
+22        return ans;     
+23    }
+24};
