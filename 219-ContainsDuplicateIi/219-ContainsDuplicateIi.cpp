32dd8e9a87c603b4d6286283392cba1c9ff89774
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-08-21 00:24:19 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/21/2026, 12:24:18 AM
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) 
+    {
+        unordered_map<int, int> mpp; 
+        for (int i=0; i<nums.size(); i++){
+            if (mpp.find(nums[i]) != mpp.end()){
+                if (abs(i - mpp[nums[i]]) <= k) return true; 
+            }
+
+            mpp[nums[i]] = i; 
+        }    
+
+        return false; 
+    }
+};
