99e3d0cf7922f493c9ee168d04a435447f161e0f
Nishant Hegde <62949127+kernelops@users.noreply.github.com>
2026-08-22 09:57:12 +0530

    Create

---
+++ problem-1787372830650/version/cpp/problem-1787372830650_20260822_095710.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/22/2026, 9:57:10 AM
+1class Solution {
+2public:
+3    bool checkDivisibility(int n) 
+4    {
+5        int sum=0, prod=1, temp=n; 
+6        while (n>0){
+7            int d = n%10; 
+8            sum += d; 
+9            prod *= d; 
+10            n /= 10; 
+11        }    
+12
+13        int val = temp / (sum+prod); 
+14        if (val * (sum+prod) == temp) return true; 
+15        return false;
+16    }
+17};
